96ea95c1c43a48e91d672fdc9fc80ab8d509d437
muhamadadam <79630124+adamnn26@users.noreply.github.com>
2022-06-09 21:58:07 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -12,7 +12,7 @@ public class Driver {
 		
 		System.out.println("Current count:" + counter.getCount());
 		
-		counter.decrement();
+		counter.decrement(2);
 		
 		System.out.println("Current count:" + counter.getCount());
 
